ca76a49f31009d5bff0a4aa4fa0eb970bdb3a2fb
shravani-av <shravaniav2@gmail.com>
2025-11-18 09:14:11 +0530

    day1 programs are uploaded

---
+++ P1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    float num=4.75;
+    printf("Size of Number=%d\n",sizeof(num));
+    printf("Size of Number=%d",sizeof(4.75));
+}
---
+++ p2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    float num= (double)((int)17.55/3);
+    printf("NUM=%d\n",num);
+    printf("NUM=%f\n",num);
+}
